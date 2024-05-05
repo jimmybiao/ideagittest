5b1269f789519b01e56099c3160f381dfbd6604f
jimmybiao <104448025@qq.com>
2024-05-05 09:44:39 +0800

    init

---
+++ src/main/java/com/jimmy/GitCommit.java
@@ -0,0 +1,16 @@
+package com.jimmy;
+
+/**
+ * ClassName: GitCommit
+ * Package: com.jimmy
+ * Description:
+ *
+ * @Author: Jimmy Zhang
+ * @Create: 2024/5/5 - 09:25
+ * @Version: v1.0
+ */
+public class GitCommit {
+    public static void main(String[] args) {
+        System.out.println("Hello, Github.");
+    }
+}
